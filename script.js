6da3b9e6cefc3c8967f1ad351f82a8366181c542
DavidAlves28 <dav.alvesc28@gmail.com>
2023-01-04 18:58:13 -0300

    carrocel-Interface

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ sr.reveal('.tituloApresentacao',{origin:'left',delay:300 ,distance:'-320px', dur
 sr.reveal('.fotoPerfil',{origin:'bottom',delay:300 ,distance:'-320px', duration:3500})
 sr.reveal('.iAm',{origin:'right',delay:300 ,distance:'-320px', duration:3000})
 sr.reveal('.imgSeta',{duration:3000})
-sr.reveal('.skillUm',{origin:'right',delay:100 ,distance:'-520px', duration:3000})
+sr.reveal('.skillUm',{origin:'right',delay:100 ,distance:'-520px', duration:2000})
 sr.reveal('.javascript',{duration:4500,delay: 500})
 sr.reveal('.css',{duration:4500,delay: 500})
 sr.reveal('.html',{duration:4500,delay: 500})
@@ -12,3 +12,4 @@ sr.reveal('.bootstrap',{duration:4500,delay: 500})
 sr.reveal('.github',{duration:4500,delay: 500})
 
 // sr.reveal('.sobre',{origin:'left',delay:100 ,distance:'-520px', duration:3000})
+
